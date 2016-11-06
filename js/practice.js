a8db2de6c29349de9c2bc762ba714eb81d477ab2
JefteMunoz <marti_mr@hotmail.com>
2016-11-05 19:13:30 -0600

    Moduralization Implementation

---
+++ js/practice.js
@@ -0,0 +1,11 @@
+$(function() {
+    $("#page-header").load("../modules/header.html");
+    $("#page-nav").load("../modules/nav.html");
+    $("#page-footer").load("../modules/footer.html");
+});
+
+$(function() {
+    $("#green-header").load("../modules/header.html");
+    $("#green-nav").load("../modules/nav.html");
+    $("#green-footer").load("../modules/footer.html");
+});
